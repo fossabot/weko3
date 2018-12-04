c2b6dc0f922248dcd597c9f48af55022bc3360bd
mhaya <masaharu.hayashi3@gmail.com>
2018-12-04 21:15:39 +0900

    small fix

--- run-tests.sh
+++ run-tests.sh
@@ -10,7 +10,7 @@
 # https://github.com/flask-admin/flask-admin/pull/1699 is merged an released.
 pipenv check --ignore 36437 && \
 pipenv run pydocstyle weko3 tests docs && \
-pipenv run isort -rc -c -df && \
+pipenv run isort -rc -c -df -s version/ -s versions/ && \
 pipenv run check-manifest --ignore ".travis-*,docs/_build*" && \
 pipenv run sphinx-build -qnNW docs docs/_build/html && \
 pipenv run test
